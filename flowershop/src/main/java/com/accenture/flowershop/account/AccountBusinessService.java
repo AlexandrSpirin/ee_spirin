99db4c825898579ed0762da4e2e11badcc16da7e
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-16 17:09:14 +0300

    Delete AccountBusinessService.java

--- flowershop/src/main/java/com/accenture/flowershop/account/AccountBusinessService.java
+++
@@ -1,6 +0,0 @@
-package com.accenture.flowershop.account;
-
-public interface AccountBusinessService {
-    boolean Login(String login, String password) throws AccountDAOException;
-    Account Registration(String login, String password, String firstName, String lastName, String email) throws AccountDAOException;
-}
