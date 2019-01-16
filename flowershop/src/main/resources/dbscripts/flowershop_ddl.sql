db694c65b0456db395336c9426e4ccaeb5b4f891
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-16 17:18:49 +0300

    Update flowershop_ddl.sql

--- flowershop/src/main/resources/dbscripts/flowershop_ddl.sql
+++ flowershop/src/main/resources/dbscripts/flowershop_ddl.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "accounts"
 (
-    "ID" NUMBER(10,0) PRIMARY KEY,
+    "ID" LONG PRIMARY KEY,
     "login" VARCHAR2(45 CHAR) NOT NULL,
     "password" VARCHAR2(45 CHAR) NOT NULL,
-    "type" VARCHAR2(1 CHAR) NOT NULL
+    "type" VARCHAR2(45 CHAR) NOT NULL
 );
 
 
@@ -11,14 +11,14 @@ CREATE TABLE "accounts"
 
 CREATE TABLE "customers"
 (
-    "ID" NUMBER(10,0) PRIMARY KEY,
-    "account_ID" NUMBER(10,0) NOT NULL,
+    "ID" LONG PRIMARY KEY,
+    "account_ID" LONG NOT NULL,
     "first_name" VARCHAR2(45 CHAR) NOT NULL,
     "middle_name" VARCHAR2(45 CHAR) NOT NULL,
     "last_name" VARCHAR2(45 CHAR) NOT NULL,
     "email" VARCHAR2(45 CHAR) NOT NULL,
     "phone_number" NUMBER(11, 0) NOT NULL,
-    "money" NUMBER(9, 2) NOT NULL,
+    "money" NUMBER(10, 2) NOT NULL,
     "discount" NUMBER(2) NOT NULL
 );
 
@@ -27,9 +27,9 @@ CREATE TABLE "customers"
 
 CREATE TABLE "flowers"
 (
-    "ID" NUMBER(10,0) PRIMARY KEY,
+    "ID" LONG PRIMARY KEY,
     "name" VARCHAR2(45 CHAR) NOT NULL,
-    "cost" NUMBER(8, 2) NOT NULL,
+    "cost" NUMBER(10, 2) NOT NULL,
 );
 
 
@@ -37,7 +37,7 @@ CREATE TABLE "flowers"
 
 CREATE TABLE "flower_pools"
 (
-    "ID" NUMBER(10,0) PRIMARY KEY,
-    "flower_ID" NUMBER(10,0) NOT NULL,
+    "ID" LONG PRIMARY KEY,
+    "flower_ID" LONG NOT NULL,
     "flower_count" NUMBER(8, 0) NOT NULL,
 );
