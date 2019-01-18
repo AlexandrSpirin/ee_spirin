1b8ac372a32f5ff8d55329b0451bb849d6b4d7f7
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-18 11:16:50 +0300

    Update AccountBusinessServiceImpl.java

--- flowershop/src/main/java/com/accenture/flowershop/be/business/AccountBusinessServiceImpl.java
+++ flowershop/src/main/java/com/accenture/flowershop/be/business/AccountBusinessServiceImpl.java
@@ -2,6 +2,7 @@ package com.accenture.flowershop.be.business;
 
 import com.accenture.flowershop.be.InternalException;
 import com.accenture.flowershop.be.access.AccountDAO;
+import com.accenture.flowershop.be.entity.account.Account;
 import com.accenture.flowershop.be.entity.account.AccountType;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +14,15 @@ import org.springframework.stereotype.Service;
 public class AccountBusinessServiceImpl implements AccountBusinessService {
 
     @Autowired
-    @Qualifier("AccountDAOImpl")
+    @Qualifier("accountDAOImpl")
     AccountDAO accountDAO;
 
     @Override
     public boolean login(String login, String password) throws InternalException {
         try {
-            if (StringUtils.equals(accountDAO.findAccount(login).getPassword(), password)) {
-                return true;
+            Account findAccount=accountDAO.findAccount(login);
+            if (findAccount!=null) {
+                return StringUtils.equals(findAccount.getPassword(), password)?true:false;
             }
             return false;
         }
