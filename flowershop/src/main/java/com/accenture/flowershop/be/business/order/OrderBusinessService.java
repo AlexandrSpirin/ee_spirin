440e4c99f6563c73fd943ecd9a0f0f2a78518378
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-21 16:43:56 +0300

    Create OrderBusinessService.java

---
+++ flowershop/src/main/java/com/accenture/flowershop/be/business/order/OrderBusinessService.java
@@ -0,0 +1,19 @@
+package com.accenture.flowershop.be.business.order;
+
+import com.accenture.flowershop.be.InternalException;
+import com.accenture.flowershop.be.entity.order.Order;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
+
+public interface OrderBusinessService {
+    List<Order> getAllOrders() throws InternalException;
+    Order findOrder(long id) throws InternalException;
+    List<Order> findOrders(String status) throws InternalException;
+    List<Order> findOrdersCreateDate(Date createDate) throws InternalException;
+    List<Order> findOrdersCloseDate(Date closeDate) throws InternalException;
+    List<Order> findOrders(BigDecimal finalPrice) throws InternalException;
+    List<Order> findOrders(Integer discount) throws InternalException;
+    boolean insertOrder(String status, Date createDate, Date closeDate, Integer discount, BigDecimal finalPrice) throws InternalException;
+}
