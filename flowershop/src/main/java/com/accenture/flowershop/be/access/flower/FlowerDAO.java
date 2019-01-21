65348aedca22b3345cd3e748092eb585c49bb77a
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-21 12:25:15 +0300

    Create FlowerDAO.java

---
+++ flowershop/src/main/java/com/accenture/flowershop/be/access/flower/FlowerDAO.java
@@ -0,0 +1,19 @@
+package com.accenture.flowershop.be.access.flower;
+
+import com.accenture.flowershop.be.InternalException;
+import com.accenture.flowershop.be.entity.flower.Flower;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface FlowerDAO {
+    List<Flower> getAllFlowers() throws InternalException;
+    Flower findFlower(long id) throws InternalException;
+    Flower findFlower(String name) throws InternalException;
+    List<Flower> findFlowers(String name) throws InternalException;
+    List<Flower> findFlowers(BigDecimal cost) throws InternalException;
+    List<Flower> findFlowersByMinCost(BigDecimal minCost) throws InternalException;
+    List<Flower> findFlowersByMaxCost(BigDecimal maxCost) throws InternalException;
+    List<Flower> findFlowersByRange(BigDecimal minCost, BigDecimal maxCost) throws InternalException;
+    boolean insertFlower(String name, BigDecimal cost) throws InternalException;
+}
