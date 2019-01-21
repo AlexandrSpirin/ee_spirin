2d70660267f19a175fb74f77c95d57386bcad604
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-21 12:22:26 +0300

    Create FlowerBusinessServiceImpl.java

---
+++ flowershop/src/main/java/com/accenture/flowershop/be/business/flower/FlowerBusinessServiceImpl.java
@@ -0,0 +1,81 @@
+package com.accenture.flowershop.be.business.flower;
+
+import com.accenture.flowershop.be.InternalException;
+import com.accenture.flowershop.be.access.flower.FlowerDAO;
+import com.accenture.flowershop.be.entity.flower.Flower;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Service
+public class FlowerBusinessServiceImpl implements FlowerBusinessService {
+
+    @Autowired
+    @Qualifier("flowerDAOImpl")
+    private FlowerDAO flowerDAO;
+
+    @Override
+    public List<Flower> getAllFlowers() throws InternalException {
+        try {
+            return flowerDAO.getAllFlowers();
+        }
+        catch (Exception e){
+            throw new InternalException(InternalException.ERROR_SERVICE_FLOWER_GET_ALL, new Throwable(e));
+        }
+    }
+
+    @Override
+    public Flower findFlower(long id) throws InternalException {
+        try {
+            return flowerDAO.findFlower(id);
+        }
+        catch (Exception e){
+            throw new InternalException(InternalException.ERROR_SERVICE_FLOWER_FIND_ID, new Throwable(e));
+        }
+    }
+
+    @Override
+    public Flower findFlower(String name) throws InternalException {
+        try {
+            return flowerDAO.findFlower(name);
+        }
+        catch (Exception e){
+            throw new InternalException(InternalException.ERROR_SERVICE_FLOWER_FIND_NAME, new Throwable(e));
+        }
+    }
+
+    @Override
+    public List<Flower> findFlowers(String name) throws InternalException {
+        try {
+            return flowerDAO.findFlowers(name);
+        }
+        catch (Exception e){
+            throw new InternalException(InternalException.ERROR_SERVICE_FLOWERS_FIND_NAME, new Throwable(e));
+        }
+    }
+
+    @Override
+    public List<Flower> findFlowers(BigDecimal cost) throws InternalException {
+        try {
+            return flowerDAO.findFlowers(cost);
+        }
+        catch (Exception e){
+            throw new InternalException(InternalException.ERROR_SERVICE_FLOWERS_FIND_COST, new Throwable(e));
+        }
+    }
+
+    @Override
+    public boolean insertFlower(String name, BigDecimal cost)
+            throws InternalException {
+        try {
+            return flowerDAO.insertFlower(name, cost);
+        }
+        catch (Exception e){
+            throw new InternalException(InternalException.ERROR_SERVICE_FLOWER_INSERT, new Throwable(e));
+        }
+    }
+}
