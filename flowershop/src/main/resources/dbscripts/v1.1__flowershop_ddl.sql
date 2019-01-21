011a02c322d095f1850e2b994a2444fc650051bf
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-21 12:03:37 +0300

    Update v1.1__flowershop_ddl.sql

--- flowershop/src/main/resources/dbscripts/v1.1__flowershop_ddl.sql
+++ flowershop/src/main/resources/dbscripts/v1.1__flowershop_ddl.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "accounts"
 (
-    "ID" LONG PRIMARY KEY,
+    "id" LONG PRIMARY KEY,
     "login" VARCHAR2(45 CHAR) NOT NULL,
     "password" VARCHAR2(45 CHAR) NOT NULL,
     "type" VARCHAR2(45 CHAR) NOT NULL
@@ -11,8 +11,8 @@ CREATE TABLE "accounts"
 
 CREATE TABLE "customers"
 (
-    "ID" LONG PRIMARY KEY,
-    "account_ID" LONG NOT NULL,
+    "id" LONG PRIMARY KEY,
+    "account_id" LONG NOT NULL,
     "first_name" VARCHAR2(45 CHAR) NOT NULL,
     "middle_name" VARCHAR2(45 CHAR) NOT NULL,
     "last_name" VARCHAR2(45 CHAR) NOT NULL,
@@ -27,9 +27,9 @@ CREATE TABLE "customers"
 
 CREATE TABLE "flowers"
 (
-    "ID" LONG PRIMARY KEY,
+    "id" LONG PRIMARY KEY,
     "name" VARCHAR2(45 CHAR) NOT NULL,
-    "cost" NUMBER(10, 2) NOT NULL,
+    "cost" NUMBER(10, 2) NOT NULL
 );
 
 
@@ -37,7 +37,18 @@ CREATE TABLE "flowers"
 
 CREATE TABLE "flower_pools"
 (
-    "ID" LONG PRIMARY KEY,
-    "flower_ID" LONG NOT NULL,
+    "id" LONG PRIMARY KEY,
+    "flower_id" LONG NOT NULL,
+    "flower_count" NUMBER(8, 0) NOT NULL
+);
+
+
+
+
+CREATE TABLE "orders"
+(
+    "id" LONG PRIMARY KEY,
+    "flower_id" LONG NOT NULL,
     "flower_count" NUMBER(8, 0) NOT NULL,
+    "discount" NUMBER(2) NOT NULL
 );
