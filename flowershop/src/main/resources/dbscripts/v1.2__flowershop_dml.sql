00142ec5df970f5e45555991f2c68a7d044545ca
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-21 12:03:59 +0300

    Update v1.2__flowershop_dml.sql

--- flowershop/src/main/resources/dbscripts/v1.2__flowershop_dml.sql
+++ flowershop/src/main/resources/dbscripts/v1.2__flowershop_dml.sql
@@ -2,55 +2,56 @@ CREATE SEQUENCE account_seq;
 CREATE SEQUENCE customer_seq;
 CREATE SEQUENCE flower_seq;
 CREATE SEQUENCE flower_pool_seq;
+CREATE SEQUENCE order_seq;
 
 
 
 
-INSERT INTO accounts (ID, login, password, type) VALUES (account_seq.NEXTVAL, 'admin','admin123','ADMIN');
+INSERT INTO accounts (id, login, password, type) VALUES (account_seq.NEXTVAL, 'admin','admin123','ADMIN');
 
 
-INSERT INTO accounts (ID, login, password, type) VALUES (account_seq.NEXTVAL, 'eaglesss','qwerty','CUSTOMER');
+INSERT INTO accounts (id, login, password, type) VALUES (account_seq.NEXTVAL, 'eaglesss','qwerty','CUSTOMER');
 
-INSERT INTO customers (ID, account_ID, first_name, middle_name, last_name, email, phone_number, money, discount) VALUES (customer_seq.NEXTVAL, account_seq.CURRVAL, 'Nikita','Aleksandrovich','Orlov', 'agamon@mail.ru', 89201542335, 100000.00, 10);
-
-
-INSERT INTO accounts (ID, login, password, type) VALUES (account_seq.NEXTVAL, 'oops','123456','CUSTOMER');
-
-INSERT INTO customers (ID, account_ID, first_name, middle_name, last_name, email, phone_number, money, discount) VALUES (customer_seq.NEXTVAL, account_seq.CURRVAL, 'Nikita','Igorevich','Popov', 'boooo@rambler.ru', 89209984445, 100.00, 1);
+INSERT INTO customers (id, account_id, first_name, middle_name, last_name, email, phone_number, money, discount) VALUES (customer_seq.NEXTVAL, account_seq.CURRVAL, 'Nikita','Aleksandrovich','Orlov', 'agamon@mail.ru', 89201542335, 100000.00, 10);
 
 
+INSERT INTO accounts (id, login, password, type) VALUES (account_seq.NEXTVAL, 'oops','123456','CUSTOMER');
+
+INSERT INTO customers (id, account_id, first_name, middle_name, last_name, email, phone_number, money, discount) VALUES (customer_seq.NEXTVAL, account_seq.CURRVAL, 'Nikita','Igorevich','Popov', 'boooo@rambler.ru', 89209984445, 100.00, 1);
 
 
-INSERT INTO flowers (ID, name, cost) VALUES (flower_seq.NEXTVAL, 'Chamomile', 10.00);
-
-INSERT INTO flower_pools (ID, flower_ID, flower_count) VALUES (flower_pool_seq.NEXTVAL, flower_seq.CURRVAL, 810);
 
 
-INSERT INTO flowers (ID, name, cost) VALUES (flower_seq.NEXTVAL, 'Jasmine', 50.00);
+INSERT INTO flowers (id, name, cost) VALUES (flower_seq.NEXTVAL, 'Chamomile', 10.00);
 
-INSERT INTO flower_pools (ID, flower_ID, flower_count) VALUES (flower_pool_seq.NEXTVAL, 2, 311);
-
-
-INSERT INTO flowers (ID, name, cost) VALUES (flower_seq.NEXTVAL, 'Chrysanthemums', 66.13);
-
-INSERT INTO flower_pools (ID, flower_ID, flower_count) VALUES (flower_pool_seq.NEXTVAL, 3, 1625);
+INSERT INTO flower_pools (id, flower_id, flower_count) VALUES (flower_pool_seq.NEXTVAL, flower_seq.CURRVAL, 810);
 
 
-INSERT INTO flowers (ID, name, cost) VALUES (flower_seq.NEXTVAL, 'Lilac', 23.42);
+INSERT INTO flowers (id, name, cost) VALUES (flower_seq.NEXTVAL, 'Jasmine', 50.00);
 
-INSERT INTO flower_pools (ID, flower_ID, flower_count) VALUES (flower_pool_seq.NEXTVAL, 4, 6234);
-
-
-INSERT INTO flowers (ID, name, cost) VALUES (flower_seq.NEXTVAL, 'Tulips', 90.00);
-
-INSERT INTO flower_pools (ID, flower_ID, flower_count) VALUES (flower_pool_seq.NEXTVAL, 5, 0);
+INSERT INTO flower_pools (id, flower_id, flower_count) VALUES (flower_pool_seq.NEXTVAL, 2, 311);
 
 
-INSERT INTO flowers (ID, name, cost) VALUES (flower_seq.NEXTVAL, 'Lotus', 141.50);
+INSERT INTO flowers (id, name, cost) VALUES (flower_seq.NEXTVAL, 'Chrysanthemums', 66.13);
 
-INSERT INTO flower_pools (ID, flower_ID, flower_count) VALUES (flower_pool_seq.NEXTVAL, 6, 1132);
+INSERT INTO flower_pools (id, flower_id, flower_count) VALUES (flower_pool_seq.NEXTVAL, 3, 1625);
 
 
-INSERT INTO flowers (ID, name, cost) VALUES (flower_seq.NEXTVAL, 'Roses', 115.00);
+INSERT INTO flowers (id, name, cost) VALUES (flower_seq.NEXTVAL, 'Lilac', 23.42);
 
-INSERT INTO flower_pools (ID, flower_ID, flower_count) VALUES (flower_pool_seq.NEXTVAL, 7, 5815);
+INSERT INTO flower_pools (id, flower_id, flower_count) VALUES (flower_pool_seq.NEXTVAL, 4, 6234);
+
+
+INSERT INTO flowers (id, name, cost) VALUES (flower_seq.NEXTVAL, 'Tulips', 90.00);
+
+INSERT INTO flower_pools (id, flower_id, flower_count) VALUES (flower_pool_seq.NEXTVAL, 5, 0);
+
+
+INSERT INTO flowers (id, name, cost) VALUES (flower_seq.NEXTVAL, 'Lotus', 141.50);
+
+INSERT INTO flower_pools (id, flower_id, flower_count) VALUES (flower_pool_seq.NEXTVAL, 6, 1132);
+
+
+INSERT INTO flowers (id, name, cost) VALUES (flower_seq.NEXTVAL, 'Roses', 115.00);
+
+INSERT INTO flower_pools (id, flower_id, flower_count) VALUES (flower_pool_seq.NEXTVAL, 7, 5815);
