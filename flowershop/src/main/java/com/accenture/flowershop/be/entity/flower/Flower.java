cf252f4799069cca5489df1ce2ae4f02c11a573c
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-21 12:17:41 +0300

    Create Flower.java

---
+++ flowershop/src/main/java/com/accenture/flowershop/be/entity/flower/Flower.java
@@ -0,0 +1,51 @@
+package com.accenture.flowershop.be.entity.flower;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+@Entity(name="flowers")
+@Table(name="flowers")
+public class Flower {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "flower_seq")
+    @SequenceGenerator(name = "flower_seq", sequenceName = "flower_seq", allocationSize = 1)
+    private long id;
+
+    private String name;
+
+    private BigDecimal cost;
+
+
+    public Flower(){}
+
+    public Flower(String name, BigDecimal cost){
+        this.name = name;
+        this.cost = cost;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+}
