01387850a344603bc8b3f56c32840dd36f493ae4
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-21 16:46:46 +0300

    Update OrderDAO.java

--- flowershop/src/main/java/com/accenture/flowershop/be/access/order/OrderDAO.java
+++ flowershop/src/main/java/com/accenture/flowershop/be/access/order/OrderDAO.java
@@ -1,17 +1,19 @@
 package com.accenture.flowershop.be.access.order;
 
 import com.accenture.flowershop.be.InternalException;
-import com.accenture.flowershop.be.entity.flower.Flower;
 import com.accenture.flowershop.be.entity.order.Order;
 
-import java.math.BigInteger;
-import java.util.HashMap;
+import java.math.BigDecimal;
+import java.sql.Date;
 import java.util.List;
 
 public interface OrderDAO {
     List<Order> getAllOrders() throws InternalException;
     Order findOrder(long id) throws InternalException;
-    List<Order> findOrders(long flowerId) throws InternalException;
+    List<Order> findOrders(String status) throws InternalException;
+    List<Order> findOrdersCreateDate(Date createDate) throws InternalException;
+    List<Order> findOrdersCloseDate(Date closeDate) throws InternalException;
+    List<Order> findOrders(BigDecimal finalPrice) throws InternalException;
     List<Order> findOrders(Integer discount) throws InternalException;
-    boolean insertOrder(HashMap<BigInteger, Integer> flowers, Integer discount) throws InternalException;
+    boolean insertOrder(String status, Date createDate, Date closeDate, Integer discount, BigDecimal finalPrice) throws InternalException;
 }
