00e8908fa754706596ed72392d3fc514b2d893a4
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-25 18:33:51 +0300

    Update OrderFlowers.java

--- flowershop/src/main/java/com/accenture/flowershop/be/entity/order/OrderFlowers.java
+++ flowershop/src/main/java/com/accenture/flowershop/be/entity/order/OrderFlowers.java
@@ -1,30 +1,59 @@
 package com.accenture.flowershop.be.entity.order;
 
+import com.accenture.flowershop.be.entity.flower.Flower;
+
 import javax.persistence.*;
 
-@Entity(name="orders_flowers")
+@Entity
 @Table(name="orders_flowers")
 public class OrderFlowers {
-    @EmbeddedId
-    private PkOrdersFlowers pkOrdersFlowers;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_flowers_seq")
+    @SequenceGenerator(name = "order_flowers_seq", sequenceName = "order_flowers_seq", allocationSize = 1)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @OneToOne
+    @JoinColumn(name = "flower_id")
+    private Flower flower;
 
     private int flowerCount;
 
 
     public OrderFlowers() {}
 
-    public OrderFlowers(PkOrdersFlowers pkOrdersFlowers, int flowerCount) {
-        this.pkOrdersFlowers = pkOrdersFlowers;
+    public OrderFlowers(Order order, Flower flower, int flowerCount) {
+        this.order = order;
+        this.flower = flower;
         this.flowerCount = flowerCount;
     }
 
 
-    public PkOrdersFlowers getPkOrdersFlowers() {
-        return pkOrdersFlowers;
+    public Long getId() {
+        return id;
     }
 
-    public void setPkOrdersFlowers(PkOrdersFlowers pkOrdersFlowers) {
-        this.pkOrdersFlowers = pkOrdersFlowers;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Flower getFlower() {
+        return flower;
+    }
+
+    public void setFlower(Flower flower) {
+        this.flower = flower;
     }
 
     public int getFlowerCount() {
