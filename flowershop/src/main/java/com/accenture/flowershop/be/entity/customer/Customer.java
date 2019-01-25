f1e73754cefef999b6be2cf91d0cd8d8ff686fe9
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-25 18:32:01 +0300

    Create Customer.java

---
+++ flowershop/src/main/java/com/accenture/flowershop/be/entity/customer/Customer.java
@@ -0,0 +1,124 @@
+package com.accenture.flowershop.be.entity.customer;
+
+import com.accenture.flowershop.be.entity.account.Account;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq")
+    @SequenceGenerator(name = "customer_seq", sequenceName = "customer_seq", allocationSize = 1)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    private BigDecimal money;
+
+    private int discount;
+
+
+    public Customer() {}
+
+    public Customer(Account account, String firstName, String middleName, String lastName, String email, String phoneNumber, BigDecimal money, int discount) {
+        this.account = account;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.money = money;
+        this.discount = discount;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+}
