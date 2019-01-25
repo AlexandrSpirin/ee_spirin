84edbe4af1f5f7ebf11a53104a373e86ed9fa634
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-25 18:16:05 +0300

    Update LoginServlet.java

--- flowershop/src/main/java/com/accenture/flowershop/fe/servlets/LoginServlet.java
+++ flowershop/src/main/java/com/accenture/flowershop/fe/servlets/LoginServlet.java
@@ -1,9 +1,13 @@
 package com.accenture.flowershop.fe.servlets;
 
 import com.accenture.flowershop.be.business.account.AccountBusinessService;
+import com.accenture.flowershop.be.business.customer.CustomerBusinessService;
 import com.accenture.flowershop.be.business.flower.FlowerBusinessService;
+import com.accenture.flowershop.be.entity.account.Account;
 import com.accenture.flowershop.be.entity.account.AccountType;
+import com.accenture.flowershop.be.entity.customer.Customer;
 import com.accenture.flowershop.fe.dto.flower.Flower;
+import com.accenture.flowershop.fe.enums.SessionAttribute;
 import com.accenture.flowershop.fe.ws.MapService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -34,48 +38,39 @@ public class LoginServlet extends HttpServlet {
     private AccountBusinessService accountBusinessService;
 
     @Autowired
-    FlowerBusinessService flowerBusinessService;
+    private CustomerBusinessService customerBusinessService;
 
     @Autowired
-    MapService mapService;
+    private FlowerBusinessService flowerBusinessService;
 
-    private ServletConfig config;
+    @Autowired
+    private MapService mapService;
 
     @Override
     public void init (ServletConfig config) throws ServletException
     {
-        this.config = config;
         super.init(config);
         SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
                 config.getServletContext());
     }
 
     @Override
-    public ServletConfig getServletConfig()
-    {
-        return config;
-    }
-
-    @Override
-    public void destroy(){}
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
     {
         login(req,resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
     {
         login(req,resp);
     }
 
 
-    public void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException
+    public void login(HttpServletRequest req, HttpServletResponse resp) throws IOException
     {
-        String login = (String) req.getParameter("login");
-        String password = (String) req.getParameter("password");
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
         PrintWriter printWriter = resp.getWriter();
         HttpSession session = req.getSession();
 
@@ -84,26 +79,32 @@ public class LoginServlet extends HttpServlet {
 
         try {
             if (accountBusinessService.login(login, password)) {
-                session.setAttribute("login", login);
+                session.setAttribute(SessionAttribute.LOGIN.toString(), login);
                 if(accountBusinessService.isAdmin(login))
                 {
-                    session.setAttribute("userType", AccountType.ADMIN);
-                    session.setAttribute("discount", "0");
-                    session.setAttribute("money", new BigDecimal(0));
+                    session.setAttribute(SessionAttribute.USER_TYPE.toString(), AccountType.ADMIN);
+                    resp.sendRedirect("flowers");
                 }
                 else {
-                    session.setAttribute("userType", AccountType.CUSTOMER);
-                    session.setAttribute("discount", "11");
-                    session.setAttribute("money", new BigDecimal(2000));
-                    HashMap<BigInteger, Integer> flowersInBasket = new HashMap<>();
-                    List<Flower> flowerDtos = new ArrayList();
-                    mapService.mapAllFlowerDtos(flowerDtos, flowerBusinessService.getAllFlowers());
-                    for (Flower f: flowerDtos) {
-                        flowersInBasket.put(BigInteger.valueOf(f.getId()), 2);
+                    session.setAttribute(SessionAttribute.USER_TYPE.toString(), AccountType.CUSTOMER);
+                    Customer customer = customerBusinessService.findCustomer(accountBusinessService.findAccount(login));
+                    if(customer != null) {
+                        session.setAttribute(SessionAttribute.SHOW_ONLY_BASKET.toString(), false);
+                        session.setAttribute(SessionAttribute.CUSTOMER.toString(), customer);
+                        HashMap<BigInteger, Integer> flowersInBasket = new HashMap<>();
+                        List<Flower> flowerDtos = new ArrayList();
+                        mapService.mapAllFlowerDtos(flowerDtos, flowerBusinessService.getAllFlowers());
+                        for (Flower f : flowerDtos) {
+                            flowersInBasket.put(BigInteger.valueOf(f.getId()), 0);
+                        }
+                        session.setAttribute(SessionAttribute.FLOWERS_IN_BASKET.toString(), flowersInBasket);
+                        resp.sendRedirect("flowers");
                     }
-                    session.setAttribute("flowersInBasket", flowersInBasket);
+                    else {
+                        printWriter.println("<h1 align=center>Customer for this Account not found!</h1>");
+                    }
                 }
-                resp.sendRedirect("flowers");
+
             }
             else {
                 printWriter.println("<h1 align=center>Login or password not correct!</h1>");
@@ -111,6 +112,7 @@ public class LoginServlet extends HttpServlet {
         }
         catch (Exception e){
             printWriter.println("<h1 align=center>Error on login!</h1>");
+            printWriter.println("<h1 align=center>" + e.getMessage() + "</h1>");
         }
 
         printWriter.println("<form action='index.jsp'>");
