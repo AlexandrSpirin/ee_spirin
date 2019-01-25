436c101d06d88a39dd475171426ce87284f1a103
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-25 18:26:10 +0300

    Update AccountBusinessService.java

--- flowershop/src/main/java/com/accenture/flowershop/be/business/account/AccountBusinessService.java
+++ flowershop/src/main/java/com/accenture/flowershop/be/business/account/AccountBusinessService.java
@@ -1,10 +1,16 @@
 package com.accenture.flowershop.be.business.account;
 
 import com.accenture.flowershop.be.InternalException;
+import com.accenture.flowershop.be.entity.account.Account;
 import com.accenture.flowershop.be.entity.account.AccountType;
+
+import java.util.List;
 
 
 public interface AccountBusinessService {
+    List<Account> getAllAccounts() throws InternalException;
+    Account findAccount(Long id) throws InternalException;
+    Account findAccount(String login) throws InternalException;
     boolean login(String login, String password) throws InternalException;
     boolean registration(String login, String password, AccountType type) throws InternalException;
     boolean isAdmin(String login) throws InternalException;
