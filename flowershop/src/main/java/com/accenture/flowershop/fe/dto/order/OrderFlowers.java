28d2f5def68672b5714b3bb56eab834e04f5c8bb
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-25 18:20:15 +0300

    Update OrderFlowers.java

--- flowershop/src/main/java/com/accenture/flowershop/fe/dto/order/OrderFlowers.java
+++ flowershop/src/main/java/com/accenture/flowershop/fe/dto/order/OrderFlowers.java
@@ -1,26 +1,49 @@
 package com.accenture.flowershop.fe.dto.order;
 
 
+import com.accenture.flowershop.fe.dto.flower.Flower;
+
 public class OrderFlowers {
-    private PkOrdersFlowers pkOrdersFlowers;
+    private Long id;
+
+    private Order order;
+
+    private Flower flower;
 
     private int flowerCount;
 
 
     public OrderFlowers() {}
 
-    public OrderFlowers(PkOrdersFlowers pkOrdersFlowers, int flowerCount) {
-        this.pkOrdersFlowers = pkOrdersFlowers;
+    public OrderFlowers(Order order, Flower flower, int flowerCount) {
+        this.order = order;
+        this.flower = flower;
         this.flowerCount = flowerCount;
     }
 
 
-    public PkOrdersFlowers getPkOrdersFlowers() {
-        return pkOrdersFlowers;
+    public Long getId() {
+        return id;
     }
 
-    public void setPkOrdersFlowers(PkOrdersFlowers pkOrdersFlowers) {
-        this.pkOrdersFlowers = pkOrdersFlowers;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Flower getFlower() {
+        return flower;
+    }
+
+    public void setFlower(Flower flower) {
+        this.flower = flower;
     }
 
     public int getFlowerCount() {
