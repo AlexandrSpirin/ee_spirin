626283dcd81445ace5f3680f678201f571afb50c
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-25 18:31:13 +0300

    Update OrderBusinessServiceImpl.java

--- flowershop/src/main/java/com/accenture/flowershop/be/business/order/OrderBusinessServiceImpl.java
+++ flowershop/src/main/java/com/accenture/flowershop/be/business/order/OrderBusinessServiceImpl.java
@@ -2,18 +2,21 @@ package com.accenture.flowershop.be.business.order;
 
 import com.accenture.flowershop.be.InternalException;
 import com.accenture.flowershop.be.access.order.OrderDAO;
+import com.accenture.flowershop.be.entity.customer.Customer;
 import com.accenture.flowershop.be.entity.order.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.util.List;
 
+@Service
 public class OrderBusinessServiceImpl implements OrderBusinessService {
     @Autowired
-    @Qualifier("flowerDAOImpl")
+    @Qualifier("orderDAOImpl")
     private OrderDAO orderDAO;
 
     @Override
@@ -22,13 +25,13 @@ public class OrderBusinessServiceImpl implements OrderBusinessService {
             return orderDAO.getAllOrders();
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_SERVICE_ORDER_GET_ALL, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_SERVICE_ORDERS_GET_ALL, new Throwable(e));
         }
     }
 
 
     @Override
-    public Order findOrder(long id) throws InternalException {
+    public Order findOrder(Long id) throws InternalException {
         try {
             return orderDAO.findOrder(id);
         }
@@ -43,7 +46,7 @@ public class OrderBusinessServiceImpl implements OrderBusinessService {
             return orderDAO.findOrders(status);
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_SERVICE_ORDER_FIND_STATUS, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_SERVICE_ORDERS_FIND_STATUS, new Throwable(e));
         }
     }
 
@@ -53,7 +56,7 @@ public class OrderBusinessServiceImpl implements OrderBusinessService {
             return orderDAO.findOrdersCreateDate(createDate);
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_SERVICE_ORDER_FIND_CREATE_DATE, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_SERVICE_ORDERS_FIND_CREATE_DATE, new Throwable(e));
         }
     }
 
@@ -63,7 +66,7 @@ public class OrderBusinessServiceImpl implements OrderBusinessService {
             return orderDAO.findOrdersCloseDate(closeDate);
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_SERVICE_ORDER_FIND_CLOSE_DATE, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_SERVICE_ORDERS_FIND_CLOSE_DATE, new Throwable(e));
         }
     }
 
@@ -73,7 +76,7 @@ public class OrderBusinessServiceImpl implements OrderBusinessService {
             return orderDAO.findOrders(finalPrice);
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_SERVICE_ORDER_FIND_FINAL_PRICE, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_SERVICE_ORDERS_FIND_FINAL_PRICE, new Throwable(e));
         }
     }
 
@@ -83,17 +86,17 @@ public class OrderBusinessServiceImpl implements OrderBusinessService {
             return orderDAO.findOrders(discount);
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_SERVICE_ORDER_FIND_DISCOUNT, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_SERVICE_ORDERS_FIND_DISCOUNT, new Throwable(e));
         }
     }
 
 
     @Override
     @Transactional
-    public boolean insertOrder(long accountId, String status, Date createDate, Date closeDate, Integer discount, BigDecimal finalPrice)
+    public boolean insertOrder(Customer customer, String status, Date createDate, Date closeDate, Integer discount, BigDecimal finalPrice)
             throws InternalException {
         try {
-            return orderDAO.insertOrder(accountId, status, createDate, closeDate, discount, finalPrice);
+            return orderDAO.insertOrder(customer, status, createDate, closeDate, discount, finalPrice);
         }
         catch (Exception e){
             throw new InternalException(InternalException.ERROR_SERVICE_ORDER_INSERT, new Throwable(e));
