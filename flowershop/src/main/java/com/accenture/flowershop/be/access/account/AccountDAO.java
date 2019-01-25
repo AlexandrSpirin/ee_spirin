acd66c9d2461cbc736ff47d223204b83113b4edf
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-25 18:21:14 +0300

    Update AccountDAO.java

--- flowershop/src/main/java/com/accenture/flowershop/be/access/account/AccountDAO.java
+++ flowershop/src/main/java/com/accenture/flowershop/be/access/account/AccountDAO.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface AccountDAO {
     List<Account> getAllAccounts() throws InternalException;
-    Account findAccount(long id) throws InternalException;
+    Account findAccount(Long id) throws InternalException;
     Account findAccount(String login) throws InternalException;
     boolean insertAccount(String login, String password, AccountType type) throws InternalException;
 }
