3661e30db6897cf1090e105647eea55461ec0108
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-25 18:25:28 +0300

    Update OrderDAOImpl.java

--- flowershop/src/main/java/com/accenture/flowershop/be/access/order/OrderDAOImpl.java
+++ flowershop/src/main/java/com/accenture/flowershop/be/access/order/OrderDAOImpl.java
@@ -1,7 +1,9 @@
 package com.accenture.flowershop.be.access.order;
 
 import com.accenture.flowershop.be.InternalException;
+import com.accenture.flowershop.be.entity.customer.Customer;
 import com.accenture.flowershop.be.entity.order.Order;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -11,6 +13,8 @@ import java.math.BigDecimal;
 import java.sql.Date;
 import java.util.List;
 
+
+@Component
 public class OrderDAOImpl implements OrderDAO{
 
     @PersistenceContext
@@ -20,19 +24,25 @@ public class OrderDAOImpl implements OrderDAO{
     public List<Order> getAllOrders() throws InternalException {
         try {
             TypedQuery<Order> q = entityManager.createQuery("SELECT o FROM orders o", Order.class);
-            List<Order> foundOrders = q.getResultList();
-            return foundOrders;
+            return q.getResultList();
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_DAO_ORDER_GET_ALL, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_DAO_ORDERS_GET_ALL, new Throwable(e));
         }
     }
 
 
     @Override
-    public Order findOrder(long id) throws InternalException {
+    public Order findOrder(Long id) throws InternalException {
         try {
-            return entityManager.find(Order.class, id);
+            TypedQuery<Order> q = entityManager.createQuery("SELECT o FROM order o WHERE o.id = :id ", Order.class);
+            q.setParameter("id", id);
+            List<Order> foundOrder = q.getResultList();
+            if(!foundOrder.isEmpty())
+            {
+                return foundOrder.get(0);
+            }
+            return null;
         }
         catch (Exception e){
             throw new InternalException(InternalException.ERROR_DAO_ORDER_FIND_ID, new Throwable(e));
@@ -44,37 +54,34 @@ public class OrderDAOImpl implements OrderDAO{
         try {
             TypedQuery<Order> q = entityManager.createQuery("SELECT o FROM orders o WHERE o.status = :d", Order.class);
             q.setParameter("s", status);
-            List<Order> foundOrders = q.getResultList();
-            return foundOrders;
+            return q.getResultList();
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_DAO_ORDER_FIND_STATUS, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_DAO_ORDERS_FIND_STATUS, new Throwable(e));
         }
     }
 
     @Override
     public List<Order> findOrdersCreateDate(Date createDate) throws InternalException {
         try {
-            TypedQuery<Order> q = entityManager.createQuery("SELECT o FROM orders o WHERE o.discount = :d", Order.class);
-            q.setParameter("cD", createDate);
-            List<Order> foundOrders = q.getResultList();
-            return foundOrders;
+            TypedQuery<Order> q = entityManager.createQuery("SELECT o FROM orders o WHERE o.createDate = :createD", Order.class);
+            q.setParameter("createD", createDate);
+            return q.getResultList();
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_DAO_ORDER_FIND_CREATE_DATE, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_DAO_ORDERS_FIND_CREATE_DATE, new Throwable(e));
         }
     }
 
     @Override
     public List<Order> findOrdersCloseDate(Date closeDate) throws InternalException {
         try {
-            TypedQuery<Order> q = entityManager.createQuery("SELECT o FROM orders o WHERE o.discount = :d", Order.class);
-            q.setParameter("cD", closeDate);
-            List<Order> foundOrders = q.getResultList();
-            return foundOrders;
+            TypedQuery<Order> q = entityManager.createQuery("SELECT o FROM orders o WHERE o.closeDate = :closeD", Order.class);
+            q.setParameter("closeD", closeDate);
+            return q.getResultList();
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_DAO_ORDER_FIND_CLOSE_DATE, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_DAO_ORDERS_FIND_CLOSE_DATE, new Throwable(e));
         }
     }
 
@@ -83,11 +90,10 @@ public class OrderDAOImpl implements OrderDAO{
         try {
             TypedQuery<Order> q = entityManager.createQuery("SELECT o FROM orders o WHERE o.final_price = :fP", Order.class);
             q.setParameter("fP", finalPrice);
-            List<Order> foundOrders = q.getResultList();
-            return foundOrders;
+            return q.getResultList();
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_DAO_ORDER_FIND_FINAL_PRICE, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_DAO_ORDERS_FIND_FINAL_PRICE, new Throwable(e));
         }
     }
 
@@ -96,21 +102,20 @@ public class OrderDAOImpl implements OrderDAO{
         try {
             TypedQuery<Order> q = entityManager.createQuery("SELECT o FROM orders o WHERE o.discount = :d", Order.class);
             q.setParameter("d", discount);
-            List<Order> foundOrders = q.getResultList();
-            return foundOrders;
+            return q.getResultList();
         }
         catch (Exception e){
-            throw new InternalException(InternalException.ERROR_DAO_ORDER_FIND_DISCOUNT, new Throwable(e));
+            throw new InternalException(InternalException.ERROR_DAO_ORDERS_FIND_DISCOUNT, new Throwable(e));
         }
     }
 
 
     @Override
     @Transactional
-    public boolean insertOrder(long accountId, String status, Date createDate, Date closeDate, Integer discount, BigDecimal finalPrice)
+    public boolean insertOrder(Customer customer, String status, Date createDate, Date closeDate, Integer discount, BigDecimal finalPrice)
             throws InternalException {
         try {
-            entityManager.persist(new Order(accountId, status, createDate, closeDate, discount, finalPrice));
+            entityManager.persist(new Order(customer, status, createDate, closeDate, discount, finalPrice));
             return true;
         }
         catch (Exception e){
