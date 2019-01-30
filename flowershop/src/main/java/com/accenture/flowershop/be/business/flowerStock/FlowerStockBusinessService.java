892ec67a5a914b1b32fed6a703c8cdc2ed03bb49
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-30 18:50:05 +0300

    Update FlowerStockBusinessService.java

--- flowershop/src/main/java/com/accenture/flowershop/be/business/flowerStock/FlowerStockBusinessService.java
+++ flowershop/src/main/java/com/accenture/flowershop/be/business/flowerStock/FlowerStockBusinessService.java
@@ -14,6 +14,6 @@ public interface FlowerStockBusinessService {
     List<FlowerStock> findFlowerStocksByMinFlowerCount(int minFlowerCount) throws InternalException;
     List<FlowerStock> findFlowerStocksByMaxFlowerCount(int maxFlowerCount) throws InternalException;
     List<FlowerStock> findFlowerStocksByRangeFlowerCount(int minFlowerCount, int maxFlowerCount) throws InternalException;
-    boolean buyFlowers(Flower flower, int flowerCount) throws InternalException;
-    boolean insertFlowerStock(Long id, Flower flower, int flowerCount, Long flowerStockId) throws InternalException;
+    boolean insertFlowerStock(Flower flower, int flowerCount) throws InternalException;
+    boolean updateFlowerStock(FlowerStock flowerStock) throws InternalException;
 }
