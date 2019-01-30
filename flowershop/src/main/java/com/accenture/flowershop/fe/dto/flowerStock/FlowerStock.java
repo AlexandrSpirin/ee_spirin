7ce00f70deeab684f559a767cc9b4b62d0205bb9
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-30 18:44:20 +0300

    Update FlowerStock.java

--- flowershop/src/main/java/com/accenture/flowershop/fe/dto/flowerStock/FlowerStock.java
+++ flowershop/src/main/java/com/accenture/flowershop/fe/dto/flowerStock/FlowerStock.java
@@ -7,19 +7,15 @@ public class FlowerStock {
 
     private Flower flower;
 
-    private int flowerCount;
-
-    private Long flowerStockId;
+    private Integer flowerCount;
 
 
 
     public FlowerStock() {}
 
-    public FlowerStock(Long id, Flower flower, int flowerCount, Long flowerStockId) {
-        this.id = id;
+    public FlowerStock(Flower flower, Integer flowerCount) {
         this.flower = flower;
         this.flowerCount = flowerCount;
-        this.flowerStockId = flowerStockId;
     }
 
 
@@ -39,19 +35,11 @@ public class FlowerStock {
         this.flower = flower;
     }
 
-    public int getFlowerCount() {
+    public Integer getFlowerCount() {
         return flowerCount;
     }
 
-    public void setFlowerCount(int flowerCount) {
+    public void setFlowerCount(Integer flowerCount) {
         this.flowerCount = flowerCount;
-    }
-
-    public Long getFlowerStockId() {
-        return flowerStockId;
-    }
-
-    public void setFlowerStockId(Long flowerStockId) {
-        this.flowerStockId = flowerStockId;
     }
 }
