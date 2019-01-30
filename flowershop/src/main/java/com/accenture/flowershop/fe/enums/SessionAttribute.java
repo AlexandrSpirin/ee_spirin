6e85a8ca8053755e095ad54fc3fe6b4419909d76
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-30 18:42:47 +0300

    Update SessionAttribute.java

--- flowershop/src/main/java/com/accenture/flowershop/fe/enums/SessionAttribute.java
+++ flowershop/src/main/java/com/accenture/flowershop/fe/enums/SessionAttribute.java
@@ -1,5 +1,5 @@
 package com.accenture.flowershop.fe.enums;
 
 public enum SessionAttribute {
-    LOGIN, CUSTOMER, USER_TYPE, FLOWERS_IN_BASKET, SHOW_ONLY_BASKET
+    LOGIN, CUSTOMER, USER_TYPE, FLOWERS_IN_BASKET, SHOW_ONLY_BASKET, SEARCH, ORDER_FOR_SHOW, SHOW_ORDERS
 }
