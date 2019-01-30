1ae870d97c01c4613e0d80c327e17504916b106c
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2019-01-30 18:49:10 +0300

    Update OrderBusinessServiceImpl.java

--- flowershop/src/main/java/com/accenture/flowershop/be/business/order/OrderBusinessServiceImpl.java
+++ flowershop/src/main/java/com/accenture/flowershop/be/business/order/OrderBusinessServiceImpl.java
@@ -3,7 +3,9 @@ package com.accenture.flowershop.be.business.order;
 import com.accenture.flowershop.be.InternalException;
 import com.accenture.flowershop.be.access.order.OrderDAO;
 import com.accenture.flowershop.be.entity.customer.Customer;
+import com.accenture.flowershop.be.entity.flower.Flower;
 import com.accenture.flowershop.be.entity.order.Order;
+import com.accenture.flowershop.be.entity.order.OrderFlowers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -29,6 +31,16 @@ public class OrderBusinessServiceImpl implements OrderBusinessService {
         }
     }
 
+
+    @Override
+    public List<OrderFlowers> getAllOrderFlowers() throws InternalException {
+        try {
+            return orderDAO.getAllOrderFlowers();
+        }
+        catch (Exception e){
+            throw new InternalException(InternalException.ERROR_SERVICE_ORDERS_GET_ALL, new Throwable(e));
+        }
+    }
 
     @Override
     public Order findOrder(Long id) throws InternalException {
@@ -103,13 +115,37 @@ public class OrderBusinessServiceImpl implements OrderBusinessService {
 
     @Override
     @Transactional
-    public boolean insertOrder(Customer customer, String status, Date createDate, Date closeDate, Integer discount, BigDecimal finalPrice)
+    public boolean insertOrderFlowers(Order order, Flower flower, Integer flowerCount)
             throws InternalException {
         try {
-            return orderDAO.insertOrder(customer, status, createDate, closeDate, discount, finalPrice);
+            return orderDAO.insertOrderFlowers(order, flower, flowerCount);
+        }
+        catch (Exception e){
+            throw new InternalException(InternalException.ERROR_SERVICE_ORDER_FLOWERS_INSERT, new Throwable(e));
+        }
+    }
+
+    @Override
+    @Transactional
+    public Order insertOrder(Customer customer, String status, List<OrderFlowers> orderFlowersList, Date createDate, Date closeDate, Integer discount, BigDecimal finalPrice)
+            throws InternalException {
+        try {
+            return orderDAO.insertOrder(customer, status, orderFlowersList, createDate, closeDate, discount, finalPrice);
         }
         catch (Exception e){
             throw new InternalException(InternalException.ERROR_SERVICE_ORDER_INSERT, new Throwable(e));
+        }
+    }
+
+    @Override
+    @Transactional
+    public boolean updateOrder(Order order)
+            throws InternalException {
+        try {
+            return orderDAO.updateOrder(order);
+        }
+        catch (Exception e){
+            throw new InternalException(InternalException.ERROR_SERVICE_ORDER_UPDATE, new Throwable(e));
         }
     }
 }
